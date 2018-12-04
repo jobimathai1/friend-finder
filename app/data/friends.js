bfd4961c104c85058d91e0bec1864a63d0ce1fae
Jobi Mathai <jobi.mathai@gmail.com>
2018-12-03 23:52:00 -0800

    added survey page

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://s7d2.scene7.com/is/image/PetSmart/PB1201_STORY_CARO-Authority-HealthyOutside-DOG-20160818?$PB1201$",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "jose",
+        "photo": "https://as1.ftcdn.net/jpg/01/28/41/60/500_F_128416052_ih5ZGWPv3CCCTsjmLBx6yB4MotvDOJzx.jpg",
+        "scores": [
+            "1",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Bella",
+        "photo": "https://images.pexels.com/photos/365500/pexels-photo-365500.jpeg",
+        "scores": [
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Roxy",
+        "photo": "https://as1.ftcdn.net/jpg/00/51/87/06/500_F_51870662_bQ7rbPgyUHgy4KAvPiNmN6TKfEzlcwi4.jpg",
+        "scores": [
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Leo",
+        "photo": "https://images.pexels.com/photos/39629/tiger-tiger-baby-tigerfamile-young-39629.jpeg",
+        "scores": [
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "felix",
+        "photo": "https://as1.ftcdn.net/jpg/02/32/02/30/500_F_232023097_pZmiLctTsHLCUorG6FvHbnqDP2eOaMHG.jpg",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
 
-var app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyparser.urlencoded({extended: true}));
